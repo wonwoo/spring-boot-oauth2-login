56ebf0b9db11a82bbf78db52b64ff00a122661c1
wonwoo <aoruqjfu@gmail.com>
2018-06-11 17:28:30 +0900

    Initial commit

---
+++ src/main/java/ml/wonwoo/springbootoauth2login/SecurityConfig.java
@@ -0,0 +1,24 @@
+package ml.wonwoo.springbootoauth2login;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests()
+        .antMatchers("/webjars/**", "/img/**").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .oauth2Login()
+        .loginPage("/login")
+        .permitAll();
+  }
+}
---
+++ src/main/java/ml/wonwoo/springbootoauth2login/SpringBootOauth2LoginApplication.java
@@ -0,0 +1,13 @@
+package ml.wonwoo.springbootoauth2login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2LoginApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootOauth2LoginApplication.class, args);
+  }
+
+}
